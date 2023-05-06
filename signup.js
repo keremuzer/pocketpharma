1e6bf57d0d36273bbc99cf0f7d24e57b207db152
Kerem Üzer <keremuzer33@gmail.com>
2023-05-06 20:16:10 +0300

    add sign in/sign up page

---
+++ signup.js
@@ -0,0 +1,50 @@
+const container = document.querySelector(".container"),
+      pwShowHide = document.querySelectorAll(".showHidePw"),
+      pwFields = document.querySelectorAll(".password"),
+      signUp = document.querySelector(".signup-link"),
+      login = document.querySelector(".login-link");
+
+    //   js code to show/hide password and change icon
+    pwShowHide.forEach(eyeIcon =>{
+        eyeIcon.addEventListener("click", ()=>{
+            pwFields.forEach(pwField =>{
+                if(pwField.type ==="password"){
+                    pwField.type = "text";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye-slash", "uil-eye");
+                    })
+                }else{
+                    pwField.type = "password";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye", "uil-eye-slash");
+                    })
+                }
+            }) 
+        })
+    })
+
+    // js code to appear signup and login form
+    signUp.addEventListener("click", ( )=>{
+        container.classList.add("active");
+    });
+    login.addEventListener("click", ( )=>{
+        container.classList.remove("active");
+    });
+
+    let bars = document.getElementById("bars");
+let active = false;
+
+bars.addEventListener("click", function(){
+  if(!active){
+    document.getElementById("mySidenav").style.width = "100%";
+    bars.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    active = true;
+  }
+  else{
+    document.getElementById("mySidenav").style.width = "0";
+    bars.innerHTML = '<i class="fa-solid fa-bars"></i>';
+    active = false;
+  }
+});
